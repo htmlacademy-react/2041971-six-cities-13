a598cddeae9b082873e8168bba2220d33fa90897
Larisa Zubkova <larissazubkova@gmail.com>
2023-07-07 17:25:15 +0300

    Добавит маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import OfferScreen from '../../pages/offer-screen/offer-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
     placesCount: number;
@@ -6,7 +13,23 @@ type AppProps = {
 
 function App({placesCount}: AppProps): JSX.Element {
   return (
-    <WelcomeScreen placesCount={placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<WelcomeScreen placesCount={placesCount} />} />
+      </Routes>
+      <Routes>
+        <Route path={AppRoute.Login} element={<LoginScreen />} />
+      </Routes>
+      <Routes>
+        <Route path={AppRoute.Favotites} element={<PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}><FavoritesScreen /></PrivateRoute>} />
+      </Routes>
+      <Routes>
+        <Route path={AppRoute.Offer} element={<OfferScreen />} />
+      </Routes>
+      <Routes>
+        <Route path="*" element={<NotFoundScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/favorites-screen/favorites-screen.tsx
+++ src/pages/favorites-screen/favorites-screen.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Logo from '../../components/logo/logo';
 import User from '../../components/user/user';
 import Sign from '../../components/sign/sign';
@@ -160,9 +161,9 @@ function FavoritesScreen(): JSX.Element {
         </div>
       </main>
       <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
+        <Link className="footer__logo-link" to="/">
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-        </a>
+        </Link>
       </footer>
     </div>
   );
