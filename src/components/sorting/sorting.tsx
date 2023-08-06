7921b809443ef76c95300e2d69beade05df1b0da
Larissa <larissazubkova@gmail.com>
2023-08-06 16:20:41 +0300

    исправит замечания проверки

--- src/components/sorting/sorting.tsx
+++ src/components/sorting/sorting.tsx
@@ -2,27 +2,27 @@ import { SortingType } from '../../const';
 import classNames from 'classnames';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { changeSortingType } from '../../store/action';
-import { useRef } from 'react';
+import { useState } from 'react';
 
 function Sorting(): JSX.Element {
-  const sortingListRef = useRef<HTMLUListElement | null>(null);
   const activeSortingType = useAppSelector((state) => state.sortingType);
   const dispatch = useAppDispatch();
+  const [isOpened, setIsOpened] = useState(false);
 
   const handleSortingClick = (type: SortingType) => {
     dispatch(changeSortingType({type}));
   };
 
   return (
-    <form className="places__sorting" action="#" method="get" onClick={() => sortingListRef.current?.classList.toggle('places__options--opened')}>
+    <form className="places__sorting" action="#" method="get" >
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span className="places__sorting-type" tabIndex={0} onClick={() => setIsOpened((option) => !option)}>
         {activeSortingType}
         <svg className="places__sorting-arrow" width="7" height="4" >
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom" ref={sortingListRef}>
+      <ul className={classNames('places__options', 'places__options--custom', {'places__options--opened':isOpened})} onClick={() => setIsOpened((option) => !option)}>
         {Object.values(SortingType).map((type) => (
           <li key={type} onClick={() => handleSortingClick(type)} className={classNames('places__option', {'places__option--active': type === activeSortingType})} tabIndex={0}>
             {type}
